baec0a7a23cceb1170f2d0f9b91a6d3f62156ea0
Richard Laws <rico@Richards-MacBook-Pro.local>
2018-10-04 00:05:36 -0700

    add counting function that replaces some numbers with ping, pong or ping-pong.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-// var userInput
+
+
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#number").val();
+    $(".results" ).empty();
+    
+    for (var i = 1; i <= userInput; i++) {
+      if (i%3===0 && i%15!=0) {
+        $(".results").append("<li>ping</li>");
+      }
+      else if (i%5===0 && i%15!=0){
+        $(".results").append("<li>pong</li>");
+      }
+      else if (i%15===0) {
+        $(".results").append("<li>PING-PONG!</li>");
+      }
+      else {
+        $(".results").append("<li>" + i + "</li>");
+      }
+    }
+
     console.log(userInput);
   });
 });
