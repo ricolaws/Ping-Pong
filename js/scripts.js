df4c111a83f0a22217b240763a00b18705c24277
Richard Laws <rico@Richards-MacBook-Pro.local>
2018-10-04 00:21:03 -0700

    add special gif and matched colors

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = $("input#number").val();
     $(".results" ).empty();
-    
+    $("#hidden").hide();
+
     for (var i = 1; i <= userInput; i++) {
       if (i%3===0 && i%15!=0) {
         $(".results").append("<li>ping</li>");
@@ -15,6 +16,7 @@ $(document).ready(function() {
       }
       else if (i%15===0) {
         $(".results").append("<li>PING-PONG!</li>");
+        $("#hidden").show();
       }
       else {
         $(".results").append("<li>" + i + "</li>");
